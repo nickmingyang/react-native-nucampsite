4a7ebb809a7542f24b2e88d9cafd8e14ed5a610d
nickming <nickmingyang@gmail.com>
2020-09-06 17:21:23 -0500

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,36 +1,67 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
-import { View, Platform } from "react-native";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { createStackNavigator } from 'react-navigation';
+import { View, Platform } from "react-native";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 
 const DirectoryNavigator = createStackNavigator(
   {
     Directory: { screen: Directory },
-    CampsiteInfo: { screen: CampsiteInfo }
+    CampsiteInfo: { screen: CampsiteInfo },
   },
   {
-    initialRouteName: 'Directory',
+    initialRouteName: "Directory",
     navigationOptions: {
       headerStyle: {
-        backgroundColor: '#5637DD'
+        backgroundColor: "#5637DD",
       },
-      headerTintColor: '#fff',
+      headerTintColor: "#fff",
       headerTitleStyle: {
-        color: '#fff'
-      }
-    }
+        color: "#fff",
+      },
+    },
   }
-)
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+  },
+  {
+    drawerBackgroundColor: "#CEC8FF",
+  }
+);
 
 class Main extends Component {
   render() {
     return (
-      <View style={{ 
+      <View
+        style={{
           flex: 1,
-          paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-        }}>
-        <DirectoryNavigator />
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <MainNavigator />
       </View>
     );
   }
