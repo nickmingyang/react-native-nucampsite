b5ffe0c781ce61b90f46b3a272bb8d926df7672b
nickming <nickmingyang@gmail.com>
2020-09-20 17:09:19 -0400

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners} from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 
 const mapDispatchToProps = {
   fetchCampsites,
@@ -142,6 +143,29 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorite: { screen: Favorites },
+  },
+  {
+    navigationOptions: ({navigation}) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: <Icon
+          name='heart'
+          type='font-awesome'
+          iconStyle={styles.stackIcon}
+          onPress={()=>navigation.toggleDrawer()}
+      />
+    }),
+  }
+);
+
 const CustomDrawerContentComponent = props => (
   <ScrollView>
       <SafeAreaView 
@@ -196,6 +220,20 @@ const MainNavigator = createDrawerNavigator(
         drawerIcon: ({tintColor})=>(
           <Icon 
             name='tree'
+            type='font-awesome'
+            size={24}
+            color={tintColor}
+          />
+        )
+      }
+    },
+    Favorites: { 
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({tintColor})=>(
+          <Icon 
+            name='heart'
             type='font-awesome'
             size={24}
             color={tintColor}
